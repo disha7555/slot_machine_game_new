5aed9616ac10d96960353ceaf151183b22983573
Disha Shah <119554768+disha7555@users.noreply.github.com>
2024-03-25 16:11:22 +0530

    Update ShowEmoji.js

--- src/ShowEmoji.js
+++ src/ShowEmoji.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Result from './Result';
 function ShowEmoji(props) {
+
+  const bstyle={backgroundColor:"#111666",color:"white",padding:"10px"};
+
   function refreshPage() { window. location. reload(false); }
   //const {updatePlay}=props;
     let emojis=[];
@@ -29,7 +32,9 @@ function ShowEmoji(props) {
     </div>
     <Result emoji1={emojis[0]} emoji2={emojis[1]} emoji3={emojis[2]} />
     <br/>
-    <button type="button" style={{marginLeft:"730px",backgroundColor:"#111666",color:"white",padding:"10px"}} onClick={refreshPage}>Play Again</button>
+    <div style={{alignItems:'center',textAlign:'center'}}>
+    <button type="button" style={bstyle} onClick={refreshPage}>Play Again</button>
+    </div>
     </>
   )
 }
